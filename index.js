98e00a003ac6796182383327c5b27682dda87ad3
CrazyAl369 <57087807+CrazyAl369@users.noreply.github.com>
2021-08-09 19:36:06 -0700

    Blog page beta code update.

---
+++ index.js
@@ -0,0 +1,36 @@
+// This is my first JavaScript code!
+
+//Output function/
+console.log('Hello World');
+
+//Variable assignment.
+let baserock = 'KJ';
+
+console.log(baserock);
+
+//Object Oriented progamming.
+let person = {
+    name: 'Adolfo',
+    age: 29
+};
+
+console.log(person);
+
+//Assigning object variable.
+person.name = 'Al';
+
+console.log(person);
+
+//Alternative way of assign object variables.
+person['name'] = 'Max';
+
+console.log(person.name);
+
+//Array test code.
+let selectedColors = ['green', 'blue']
+
+console.log(selectedColors);
+
+selectedColors[20] = 'orange';
+
+console.log(selectedColors);
